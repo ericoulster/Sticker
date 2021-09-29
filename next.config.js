0ee568c6d52d1d8062920050af39951d41b39f09
Kyle Fontenot <kwfonten@asu.edu>
2021-09-28 18:09:50 -0700

    First commit.

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
+const isProd = process.env.NODE_ENV === 'production'
+ // still may need editing and adjusting
 module.exports = {
   reactStrictMode: true,
+  assetPrefix: isProd ? '/github.com/ericoulster/Sticker/' : ''
 }
